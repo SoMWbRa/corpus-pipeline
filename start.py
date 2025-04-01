831a5cd4cdbe70471806f3a24869640328e49e15
horielov.d <horielov.d@gmail.com>
2025-04-01 17:25:47 +0300

    removed unused code

--- start.py
+++ start.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
 from sources.pipeline.suspilne_pipeline import SuspilnePipeline
-from sources.utils.daterange import daterange
 
 if __name__ == "__main__":
     pipeline = SuspilnePipeline()
