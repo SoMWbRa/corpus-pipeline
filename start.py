94bf6f0c0f0cb819ea1097c18b91515cc55f73f7
horielov.d <horielov.d@gmail.com>
2025-06-09 11:35:05 +0300

    deleted all files

--- start.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, timedelta
-from sources.pipeline.suspilne_pipeline import SuspilnePipeline
-
-if __name__ == "__main__":
-    pipeline = SuspilnePipeline()
-    yesterday = datetime.now() - timedelta(days=1)
-
-    pipeline.execute(start=yesterday, end=datetime.now())
